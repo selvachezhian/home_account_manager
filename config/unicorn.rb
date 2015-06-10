936aa8249e394cafa74693413baf0293fd785066
Selva Chezhian <selvachezhian.labam@gmail.com>
2015-06-10 12:02:03 +0530

    unicorn configuration added

---
+++ config/unicorn.rb
@@ -0,0 +1,31 @@
+app_dir = File.expand_path("../..", __FILE__)
+
+worker_processes 3
+timeout 15
+preload_app true
+
+# before_fork do |server, worker|
+#   Signal.trap 'TERM' do
+#     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+#     Process.kill 'QUIT', Process.pid
+#   end
+#
+#   defined?(ActiveRecord::Base) and
+#       ActiveRecord::Base.connection.disconnect!
+# end
+#
+# after_fork do |server, worker|
+#   Signal.trap 'TERM' do
+#     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+#   end
+#
+#   defined?(ActiveRecord::Base) and
+#       ActiveRecord::Base.establish_connection
+# end
+
+# Logging
+stderr_path "#{app_dir}/log/unicorn.stderr.log"
+stdout_path "#{app_dir}/log/unicorn.stdout.log"
+
+# Set master PID location
+pid "#{app_dir}/tmp/pids/unicorn.pid"
