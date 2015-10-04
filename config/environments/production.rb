95166821f817f1444b819c01f3aabaa27768c250
Selva Chezhian <selvachezhian.labam@gmail.com>
2015-10-04 14:41:14 +0530

    production email smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'https://manage-account.herokuapp.com' }
+
+  # Mail configurations
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.smtp_settings         = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'meetups.listertechnologies.com',
+      user_name:            'meetup.lister@gmail.com',
+      password:             'meetup@lister',
+      authentication:       :plain,
+      enable_starttls_auto: true }
 end
