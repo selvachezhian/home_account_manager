d8a4327d04f3c803921715aa0d9d1d95b8c2b83f
Selva Chezhian <selvachezhian.labam@gmail.com>
2015-11-13 23:13:04 +0530

    expenditure type order in form changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,15 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Mail configurations
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.smtp_settings         = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'meetups.listertechnologies.com',
+      user_name:            'meetup.lister@gmail.com',
+      password:             'meetup@lister',
+      authentication:       :plain,
+      enable_starttls_auto: true }
 end
